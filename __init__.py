70354579727841501e138a8c6dfa0573618276db
Ross Goodwin <ross.goodwin@gmail.com>
2015-04-08 17:45:16 -0400

    license

--- __init__.py
+++ __init__.py
@@ -1,3 +1,27 @@
+# word.camera image-to-text translator
+# Copyright  (C) 2015  Ross Goodwin
+#  
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+# You can contact Ross Goodwin at ross.goodwin@gmail.com or address
+# physical correspondence to:
+#  
+# Ross Goodwin c/o ITP
+# 721 Broadway
+# 4th Floor
+# New York, NY 10003
+
 from py.client import ClarifaiApi
 import random
 from random import choice as rc
